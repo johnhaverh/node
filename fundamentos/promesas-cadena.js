6c3020f06beef4abb09b747f2724f343c2f9cebc
John Haver Herrera <johnhaverh@gmail.com>
2023-09-14 14:34:41 -0500

    use promise & promise-string concept

---
+++ fundamentos/promesas-cadena.js
@@ -0,0 +1,84 @@
+const empleados = [
+    {
+        id: 1,
+        nombre: 'John'
+    },
+    {
+        id: 2,
+        nombre: 'Isabella'
+    },
+    {
+        id: 3,
+        nombre: 'Carolina'
+    },
+];
+
+const salarios = [
+    {
+        id: 1,
+        salario: 1000
+    },
+    {
+        id: 2,
+        salario: 1500
+    }
+];
+
+
+// const getEmpleado = (id) => {
+    //     const promesa = new Promise((resolve, reject) => {
+        //         const empleado = empleados.find (e => e.id === id)?.nombre;
+        //         if (empleado){
+            //             resolve(empleado);
+            //         } else {
+                //             reject(`Empleado con id ${id} no existe`);
+                //         }
+                //     });
+                //     return promesa;
+                // }
+                
+                // const getEmpleado = (id) => {
+                    //     return new Promise((resolve, reject) => {
+                        //         const empleado = empleados.find (e => e.id === id)?.nombre;
+                        //         if (empleado){
+//             resolve(empleado);
+//         } else {
+//             reject(`Empleado con id ${id} no existe`);
+//         }
+//     });
+// }
+
+const getEmpleado = (id) => {
+    return new Promise((resolve, reject) => {
+        const empleado = empleados.find (e => e.id === id)?.nombre;
+        (empleado) ?  resolve(empleado) : reject(`Empleado con id ${id} no existe`);
+    });
+}
+
+const getSalario = (id) => {
+    return new Promise((resolve, reject) => {
+        const salario = salarios.find (s => s.id === id)?.salario;
+        (salario) ?  resolve(salario) : reject(`Salario del empleado con id ${id} no existe`);
+    });
+}
+
+
+const id=4;
+
+// getEmpleado(id)
+//     .then(empleado => console.log(empleado))
+//     .catch(err => console.log(err));
+
+// getSalario(id)
+//     .then(salario => console.log(salario))
+//     .catch(err => console.log(err));
+
+let nombre;
+
+getEmpleado(id)
+    .then(empleado => {
+        nombre = empleado;
+        return getSalario(id);
+    })
+    .then(salario => console.log('El empleado:',nombre, 'tiene un salario de:',salario))
+    .catch(err => console.log(err));
