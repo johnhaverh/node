2c31394874b9dbd632d9dd397c1aba22240daa98
John Haver Herrera <johnhaverh@gmail.com>
2023-09-14 07:52:24 -0500

    add first file

---
+++ app.js
@@ -0,0 +1,14 @@
+let nombre = 'John';
+console.clear();
+console.log(nombre);
+
+nombre = 'Juan';
+console.log(nombre);
+
+const nombre2='Isabella';
+console.log(nombre2);
+const nombre3='Carolina';
+console.log(nombre3);
+
+console.log('Hola Mundo');
+
